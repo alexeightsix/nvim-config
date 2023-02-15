63abd5e820ed2bc68b5ff50e48e46ce9735cfc85
Alexander Latour <alexlatour@gmail.com>
2023-02-15 18:16:40 -0500

    replaced packer with lazy vim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,202 +1,184 @@
-local cmd = vim.cmd
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
-local packer_bootstrap = ensure_packer()
+vim.opt.rtp:prepend(lazypath)
 
-cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
+require("lazy").setup({
+  -- Improve startup time for Neovim
+  {
+    priority = 4000,
+    "lewis6991/impatient.nvim",
+    config = function()
+      require("impatient")
+    end,
+    lazy = false,
+  },
+  "nvim-tree/nvim-web-devicons",
+  -- Dracula colorscheme for neovim written in Lua
+  {
+    priority = 5000,
+    "Mofiqul/dracula.nvim",
+    config = function()
+      vim.cmd([[colorscheme dracula]])
+    end,
+    lazy = false,
+  },
 
-return require("packer").startup({
-  function(use)
-    -- A use-package inspired plugin manager for Neovim.
-    -- Uses native packages, supports Luarocks dependencies,
-    -- written in Lua, allows for expressive config
-    use({ "wbthomason/packer.nvim", opt = true })
+  -- A blazing fast and easy to configure neovim statusline
+  -- plugin written in pure lua.
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+    config = function()
+      require("plugins.lualine")
+    end,
+  },
 
-    -- Improve startup time for Neovim
-    use({
-      "lewis6991/impatient.nvim",
-      config = function()
-        require("impatient")
-      end,
-    })
+  -- Find, Filter, Preview, Pick. All lua, all the time.
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "0.1.x",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+    lazy = false,
+  },
+  -- boom Create key bindings that stick. WhichKey is a lua
+  -- plugin for Neovim 0.5 that displays a popup with possible
+  -- keybindings of the command you started typing.
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("plugins.whichkey")
+    end,
+    lazy = true,
+  },
 
-    use("nvim-tree/nvim-web-devicons")
-    -- Dracula colorscheme for neovim written in Lua
-    use({
-      "Mofiqul/dracula.nvim",
-      config = function()
-        vim.cmd([[colorscheme dracula]])
-      end,
-    })
+  { "romgrk/barbar.nvim", dependencies = "nvim-web-devicons" },
 
-    -- A blazing fast and easy to configure neovim statusline
-    -- plugin written in pure lua.
-    use({
-      "nvim-lualine/lualine.nvim",
-      requires = { "kyazdani42/nvim-web-devicons", opt = true },
-      config = function()
-        require("plugins.lualine")
-      end,
-    })
-
-    -- Find, Filter, Preview, Pick. All lua, all the time.
-    use({
-      "nvim-telescope/telescope.nvim",
-      tag = "0.1.x",
-      requires = { { "nvim-lua/plenary.nvim" } },
-    })
-
-    -- boom Create key bindings that stick. WhichKey is a lua
-    -- plugin for Neovim 0.5 that displays a popup with possible
-    -- keybindings of the command you started typing.
-    use({
-      "folke/which-key.nvim",
-      config = function()
-        require("plugins.whichkey")
-      end,
-    })
-
-    use({ "romgrk/barbar.nvim", requires = "nvim-web-devicons" })
-
-    use({
-      "nvim-tree/nvim-tree.lua",
-      config = function()
-        require("plugins.nvim_tree")
-      end,
-      requires = {
-        "nvim-tree/nvim-web-devicons",
-      },
-    })
-
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-
-    use("mbbill/undotree")
-
-    use({
-      "windwp/nvim-autopairs",
-      config = function()
-        require("plugins.autopairs")
-      end,
-    })
-
-    use({
-      "lewis6991/gitsigns.nvim",
-      config = function()
-        require("plugins.gitsigns")
-      end,
-    })
-
-    use({
-      "lukas-reineke/indent-blankline.nvim",
-      config = function()
-        require("plugins.blanklines")
-      end,
-    })
-
-    use({
-      "numToStr/Comment.nvim",
-      config = function()
-        require("plugins.comment")
-      end,
-    })
-
-    use({
-      "RRethy/vim-illuminate",
-      config = function()
-        require("plugins.illuminate")
-      end,
-    })
-
-    use({
-      branch = "v1.x",
-      "VonHeikemen/lsp-zero.nvim",
-      config = function()
-        require("plugins.lsp-zero")
-        require("plugins.luasnip")
-      end,
-      requires = {
-        { "L3MON4D3/LuaSnip" },
-        { "hrsh7th/cmp-buffer" },
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/cmp-nvim-lua" },
-        { "hrsh7th/cmp-path" },
-        { "hrsh7th/nvim-cmp" },
-        { "neovim/nvim-lspconfig" },
-        { "rafamadriz/friendly-snippets" },
-        { "saadparwaiz1/cmp_luasnip" },
-        { "williamboman/mason-lspconfig.nvim" },
-        { "williamboman/mason.nvim" },
-      },
-    })
-
-    use({
-      "folke/trouble.nvim",
-      config = function()
-        require("plugins.trouble")
-      end,
-    })
-
-    use({
-      "ahmedkhalf/project.nvim",
-      config = function()
-        require("plugins.project")
-      end,
-    })
-
-    use("matze/vim-move")
-
-    use({
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require("plugins.null-ls")
-      end,
-    })
-
-    use("farmergreg/vim-lastplace")
-
-    use({
-      "petertriho/nvim-scrollbar",
-      config = function()
-        require("plugins.scrollbar")
-      end,
-    })
-
-    use({
-      "rmagatti/auto-session",
-      config = function()
-        require("plugins.auto-session")
-      end,
-    })
-
-    use({
-      "yamatsum/nvim-cursorline",
-      config = function()
-        require("nvim-cursorline")
-      end,
-    })
-    use("editorconfig/editorconfig-vim")
-    if packer_bootstrap then
-      require("packer").sync()
-    end
-  end,
-  config = {
-    display = {
-      open_fn = require("packer.util").float,
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("plugins.nvim_tree")
+    end,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
     },
   },
+
+  { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
+
+  { "mbbill/undotree" },
+
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("plugins.autopairs")
+    end,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("plugins.gitsigns")
+    end,
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("plugins.blanklines")
+    end,
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("plugins.comment")
+    end,
+  },
+
+  {
+    "RRethy/vim-illuminate",
+    config = function()
+      require("plugins.illuminate")
+    end,
+  },
+
+  {
+    branch = "v1.x",
+    "VonHeikemen/lsp-zero.nvim",
+    config = function()
+      require("plugins.lsp-zero")
+      require("plugins.luasnip")
+    end,
+    dependencies = {
+      { "L3MON4D3/LuaSnip" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/nvim-cmp" },
+      { "neovim/nvim-lspconfig" },
+      { "rafamadriz/friendly-snippets" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "williamboman/mason-lspconfig.nvim" },
+      { "williamboman/mason.nvim" },
+    },
+  },
+
+  {
+    "folke/trouble.nvim",
+    config = function()
+      require("plugins.trouble")
+    end,
+  },
+
+  {
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("plugins.project")
+    end,
+  },
+
+  "matze/vim-move",
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      require("plugins.null-ls")
+    end,
+  },
+
+  { "farmergreg/vim-lastplace" },
+
+  {
+    "petertriho/nvim-scrollbar",
+    config = function()
+      require("plugins.scrollbar")
+    end,
+  },
+
+  {
+    "rmagatti/auto-session",
+    config = function()
+      require("plugins.auto-session")
+    end,
+  },
+
+  {
+    "yamatsum/nvim-cursorline",
+    config = function()
+      require("nvim-cursorline")
+    end,
+  },
+  "editorconfig/editorconfig-vim",
 })
