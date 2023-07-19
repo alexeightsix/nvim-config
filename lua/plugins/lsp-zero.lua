7a891cb020d58d3b6d5daac7dab01b22fc57edb3
Alexander Latour <alexlatour@gmail.com>
2023-07-19 07:57:44 -0400

    sync

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -3,22 +3,22 @@ local cmp = require("cmp")
 local lspkind = require("lspkind")
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-  "bashls",
-  "cssls",
-  "dockerls",
-  "emmet_ls",
-  "gopls",
-  "html",
-  "intelephense",
-  "jsonls",
-  "pyright",
-  "tailwindcss",
-  "theme_check",
-  "tsserver",
-  "vimls",
-  "yamlls",
-})
+-- lsp.ensure_installed({
+--   "bashls",
+--   "cssls",
+--   "dockerls",
+--   "emmet_ls",
+--   "gopls",
+--   "html",
+--   "intelephense",
+--   "jsonls",
+--   "pyright",
+--   "tailwindcss",
+--   "theme_check",
+--   "tsserver",
+--   "vimls",
+--   "yamlls",
+-- })
 
 lsp.nvim_workspace()
 
