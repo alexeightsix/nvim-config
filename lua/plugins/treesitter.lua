b71efd981ed388ad0bd5a8b2662fc564944e6a70
Alexander Latour <alexlatour@gmail.com>
2024-02-24 13:44:02 -0500

    blade

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,21 +6,19 @@ require('nvim-treesitter.configs').setup({
   }
 })
 
-vim.cmd([[
-lua <<EOF
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.blade = {
   install_info = {
     url = "https://github.com/EmranMR/tree-sitter-blade",
-    files = {"src/parser.c"},
+    files = { "src/parser.c" },
     branch = "main",
   },
   filetype = "blade"
 }
-EOF
 
-" Set the *.blade.php file to be filetype of blade
-augroup BladeFiltypeRelated
-  au BufNewFile,BufRead *.blade.php set ft=blade
-augroup END
-]])
+vim.cmd [[
+  augroup BladeFiltypeRelated
+    au BufNewFile,BufRead *.blade.php set ft=blade
+  augroup END
+]]
+
